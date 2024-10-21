7fbfced72b236b4bd81f6e44eaf852357f967a1a
Faf-dev <noaillesjulien@gmail.com>
2024-10-21 14:10:02 +0000

    pushing task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - blabla
+ * Return: return 0
+ **/
+int main(void)
+{
+int lettre;
+int lign = '\n';
+for (lettre = 'a'; lettre <= 'z'; lettre++)
+	putchar(lettre);
+	putchar(lign);
+	return (0);
+}
+
