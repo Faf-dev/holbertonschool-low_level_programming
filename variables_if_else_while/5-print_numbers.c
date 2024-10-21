d8ae563bcc49f5c02e6772476dfb8ea9f94f77cc
Faf-dev <noaillesjulien@gmail.com>
2024-10-21 17:47:44 +0000

    push task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - blabla
+ * Return: return 0
+ **/
+int main(void)
+{
+	int chiffre;
+
+	for (chiffre = 0; chiffre <= 9; chiffre++)
+		printf("%d", chiffre);
+	printf("\n");
+	return (0);
+}
