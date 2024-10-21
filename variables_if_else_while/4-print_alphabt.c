ad1393cd6a6ffdb869987fc68b392e9f02d28a93
Faf-dev <noaillesjulien@gmail.com>
2024-10-21 17:26:51 +0000

    push task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - blabla
+ * Return: return 0
+ **/
+
+int main(void)
+{
+	int lettre;
+	int lign = '\n';
+
+	for (lettre = 'a'; lettre <= 'z'; lettre++)
+		if (lettre == 'q' || lettre == 'e')
+		{
+			continue;
+		}
+	putchar(lettre);
+		putchar(lign);
+	return (0);
+}
