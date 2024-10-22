b07e8c3fc27095f6f0322a15130fcd822bac097f
Faf-dev <noaillesjulien@gmail.com>
2024-10-22 18:06:47 +0000

    push task 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * _isalpha - check the code.
+ *
+ * @c: variable
+ *
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+{
+	return ((c != 'c') || (c != 'C'));
+}
