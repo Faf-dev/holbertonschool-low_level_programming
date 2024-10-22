1e5e6d74ef9772ee4b48d1f0eaf4127173742627
Faf-dev <noaillesjulien@gmail.com>
2024-10-22 13:32:17 +0000

    jpp

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -3,12 +3,16 @@
 #include "main.h"
 #include <unistd.h>
 
+/**
+ * main - wewe
+ * return: return 0
+ */
 void print_alphabet_x10(void)
 
 {
 int a;
 char c;
-
+i
 for (a = 0; a < 10; a++)
 {
 	for (c = 'a'; c <= 'z'; c++)
