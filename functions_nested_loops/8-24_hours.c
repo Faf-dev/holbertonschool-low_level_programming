0558b958f7b598d38c1cb0864df47131eab570cf
Faf-dev <noaillesjulien@gmail.com>
2024-10-22 21:16:01 +0000

    push task 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - main code
+ *
+ * @hour hour count
+ *
+ * @minutes; minutes count
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	int hour, minutes;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minutes = 0; minutes <= 59; minutes++)
+		{
+		_putchar((hour / 10) + '0');
+		_putchar((hour % 10) + '0');
+		_putchar(':');
+		_putchar((minutes / 10) + '0');
+		_putchar((minutes % 10) + '0');
+		_putchar('\n');
+	}
+	}
+}
