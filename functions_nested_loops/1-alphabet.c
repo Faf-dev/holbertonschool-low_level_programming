fe9dbbac49baf2f0b01577db256e09110f914c55
Faf-dev <noaillesjulien@gmail.com>
2024-10-22 09:11:44 +0000

    push task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_alphabet - wewewe
+ * main - nique ta mere
+ * return: return 0
+ */
+void print_alphabet(void)
+
+{
+	int alpha;
+
+	for (alpha = 'a'; alpha < 'z'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,11 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-/**
- *main - sert a r
- *Return: return 0
- */
-int coucou(void)
-{
-	printf("niquezebi\n");
-	return (0);
-}
+
+void print_alphabet(void);
+
