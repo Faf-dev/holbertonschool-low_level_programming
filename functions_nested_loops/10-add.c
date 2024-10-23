8466fa89623eedb4c08748cf93d8016002824553
Faf-dev <noaillesjulien@gmail.com>
2024-10-23 11:50:17 +0000

    push task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - main code
+ *
+ * @a: number to multiple
+ *
+ * @b: number of multiplication needed
+ *
+ * Return: always 0 table of multiple
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
