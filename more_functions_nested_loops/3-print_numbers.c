d14242c509513f8184fa679ad27a1d66f34f9736
Faf-dev <noaillesjulien@gmail.com>
2024-10-28 10:57:54 +0000

    push task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - main code
+ *
+ * @num: number
+ *
+ * Return: return always 0
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	_putchar(num);
+	_putchar('\n');
+}
