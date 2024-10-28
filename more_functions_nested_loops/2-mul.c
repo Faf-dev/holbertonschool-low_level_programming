d66c41800d21091f52c5c247412702be3ef3ac10
Faf-dev <noaillesjulien@gmail.com>
2024-10-28 10:40:36 +0000

    push task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiply
+ *
+ * @a: fist number
+ *
+ * @b: second number
+ *
+ * Return: return the multiply
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
