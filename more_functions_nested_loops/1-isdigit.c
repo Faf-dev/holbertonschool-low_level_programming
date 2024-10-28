fbe80db527dec018169ef47859164268cd69e428
Faf-dev <noaillesjulien@gmail.com>
2024-10-28 10:26:11 +0000

    push  task 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if it's a digit
+ *
+ * @c: variable
+ *
+ * Return: return 1 if it's a digit
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '1')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
