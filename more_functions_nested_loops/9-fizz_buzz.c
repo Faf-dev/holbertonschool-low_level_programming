48739027c9843298489524338660df61dc77ed3c
Faf-dev <noaillesjulien@gmail.com>
2024-10-29 09:53:00 +0000

    push task 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - main code
+ *
+ * return: always 0
+ */
+
+int main (void)
+{
+	int chiffre;
+
+	for (chiffre = 1; chiffre <= 100; chiffre++)
+	{
+		if (((chiffre % 5) == 0) && ((chiffre % 3) == 0))
+			printf("FizzBuzz");
+		else if ((chiffre % 3) == 0)
+			printf("Fizz");
+		else if ((chiffre % 5) == 0)
+			printf("Buzz");
+		else
+			printf("%d", chiffre);
+		if (chiffre < 100)
+			putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
