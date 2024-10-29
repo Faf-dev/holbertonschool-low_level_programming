cdbd2c50beb4e3a214c8cb8e1440b77a91fa56bf
Faf-dev <noaillesjulien@gmail.com>
2024-10-29 12:51:12 +0000

    push task 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * swap_int - main code
+ *
+ * @*a : first value
+ * @*b : second value
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temporaire = *a;
+	*a = *b;
+	*b = temporaire;
+}
