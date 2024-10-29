7fe21e09d9102f57d50918b63cd53c20b8402244
Faf-dev <noaillesjulien@gmail.com>
2024-10-29 13:10:48 +0000

    test task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - main code
+ *
+ * @s : pointer to the string
+ *
+ * Return: always 0
+ */
+
+int _strlen(char *s)
+{
+	return (*s);
+}
