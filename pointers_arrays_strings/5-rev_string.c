972dc6425d3d36bdc47cb3d8f22d2990128756dd
Faf-dev <noaillesjulien@gmail.com>
2024-10-31 10:24:05 +0000

    fix task 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -11,15 +11,17 @@
 
 void rev_string(char *s)
 {
+	char temporaire;
 	int longueur = 0;
-	int reverse;
+	int first_half;
+	int scd_half = 0;
 
 	while (*s != '\0')
-	{
 		longueur++;
-	}
-	for (reverse = longueur - 1; reverse >= 0; reverse--)
+	for (first_half = longueur - 1; first_half >= longueur; first_half--)
 	{
-
+		temporaire = s[first_half];
+		s[first_half] = s[scd_half];
+		s[scd_half] = temporaire;
+		scd_half++;
 	}
-}
