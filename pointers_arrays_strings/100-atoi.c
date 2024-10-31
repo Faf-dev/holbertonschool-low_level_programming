881cfac15c4303ea5eed10ff502157df1fd4344b
Faf-dev <noaillesjulien@gmail.com>
2024-10-31 12:12:14 +0000

    make task 7 more readable

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,13 +9,5 @@
 int _atoi(char *s)
 {
 	int longueur;
-
-	for (s[longueur] = 0; longueur < s; longueur++)
-	{
-		while (s[longueur] > 48 && s[longueur] < 57)
-		{
-			_putchar(s[longueur]);
-		}
-	}
 	return (0);
 }
--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -10,13 +10,13 @@
 void puts_half(char *str)
 {
 	int lon = 0;
-	int first_lon;
+	int first_half;
 
 	while (str[lon] != '\0')
 		lon++;
-	for (first_lon = lon / 2; first_lon < lon; first_lon++)
+	for (first_half = lon / 2; first_half < lon; first_half++)
 	{
-	_putchar(str[first_lon]);
+	_putchar(str[first_half]);
 	}
 	_putchar('\n');
 }
