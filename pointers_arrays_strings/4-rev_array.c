7cf5553a1f961c52b9a90841cbcfccd4f3ccaa1d
Faf-dev <noaillesjulien@gmail.com>
2024-11-01 16:17:04 +0000

    push task 4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - print an array of integers
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int temporaire;
+	int i;
+
+	for (i = 0; i < (n / 2); i++)
+	{
+		temporaire = a[i];
+		a[i] = a[n(-i - 1)];
+		a[n(-i - 1)] = temporaire;
+	}
+}
