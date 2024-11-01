734982bb57e86aaec03f8b57f4bcd6a4be17569d
Faf-dev <noaillesjulien@gmail.com>
2024-11-01 15:01:02 +0000

    taking some point with task 3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcmp - check the code
+ *
+ * @s1 : first
+ *
+ * @s2 : second
+ *
+ * Return: Always 0.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	if (s1 == s2)
+	{
+		return (0);
+	}
+	return (0);
+}
