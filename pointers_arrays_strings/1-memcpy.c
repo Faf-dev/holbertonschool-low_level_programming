e250cf41b6ca5e17fc48b0c27b585a04ecedadca
Faf-dev <noaillesjulien@gmail.com>
2024-11-04 14:47:17 +0000

    patch task 1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -5,19 +5,17 @@
  * _memcpy - prints buffer in hexa
  * @dest : the address of memory to print
  * @src : the size of the memory to print
+ * @n : number of caractere to print
  *
  * Return: Dest.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	while (n != 0)
+	int i;
+
+	for (i = 0; i < n; i++)
 	{
-		if (src < n)
-		{
-			n = src;
-		}
-		dest[n - 1] = src[n - 1];
-		n--;
+		dest[i] = src[i];
 	}
 	return (dest);
 }
