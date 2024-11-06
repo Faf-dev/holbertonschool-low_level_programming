7f208355dbc9d2aef5fc4f27e8632729887f4ea2
Faf-dev <noaillesjulien@gmail.com>
2024-11-06 12:48:29 +0000

    fix task 4 p 2

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * main - multiplie deux argc
@@ -12,16 +13,33 @@
 
 int main(int argc, char **argv)
 {
-	int i;
+	int i; 
+	unsigned int j;
 	int addition = 0;
+	char *check;
 
 	if (argc > 1)
 	{
-		for (i = 1; argv[i]; i++)
+		for (i = 1; i < argc; i++)
 		{
-			addition += atoi(argv[i]);
+			check = argv[i];
+
+			for (j = 0; j < strlen(check); j++) /* parcours check */
+			{
+			if (check[j] < 48 || check[j] > 57) /* verifie si chaque valeur de check est un decimal */
+			{
+				printf("Error\n"); 
+				return (1);
+			}
+			addition += atoi(check); /* converti char en int et additionne chaque valeur */
+			check++; /* incrementation de check pour passer au caractere suivant */
 		}
 	}
 		printf("%d\n", addition);
+	}
+	else
+	{
+		printf("0\n"); /* si pas d'entier, print 0 et retour ligne */
+	}
 		return (0);
 }
