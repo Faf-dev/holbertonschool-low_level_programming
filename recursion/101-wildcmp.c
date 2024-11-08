f7cb57d4d0540374bf557ac93e00ee8b1d5f1255
Faf-dev <noaillesjulien@gmail.com>
2024-11-08 18:23:58 +0000

    push task 101

---
+++ recursion/101-wildcmp.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * wildcmp - check is the string is the same
+ * @s1 : the string to analyse
+ * @s2 : the second string to analyse
+ * Return: recursive
+ */
+int wildcmp(char *s1, char *s2)
+{
+	s2 = s1;
+	
+	if (*s1 != '\0')
+	{
+		wildcmp(s1 + 1, s2 + 1);
+	}
+	if (*s1 == '\0')
+	{
+		if (*s1 == *s2)
+			return (1);
+		else
+			return (0);
+	}
+	return (1);
+}
