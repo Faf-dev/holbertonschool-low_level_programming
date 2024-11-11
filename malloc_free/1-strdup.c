6c2aac82aaf9c9485e6a940a3fd34438359f9277
Faf-dev <noaillesjulien@gmail.com>
2024-11-11 14:04:02 +0000

    push task 1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -9,5 +9,27 @@
  */
 char *_strdup(char *str)
 {
-	char *strcpy = malloc(
+	unsigned int i = 0;
+	char *strcpy;
+	unsigned int i_malloc;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	strcpy = malloc((i + 1) * 1);
+	if (strcpy == NULL)
+	{
+		return (NULL);
+	}
+	for (i_malloc = 0; i_malloc < i; i_malloc++)
+	{
+		strcpy[i_malloc] = str[i_malloc];
+	}
+	strcpy[i_malloc] = '\0';
+	return (strcpy);
 }
