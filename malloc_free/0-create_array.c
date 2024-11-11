f3bbd8bd95330b9c01e0340e90021f6c2a9fee9e
Faf-dev <noaillesjulien@gmail.com>
2024-11-11 13:20:59 +0000

    push all empty files + task 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creer un tableau de caractere
+ * @size : taille de l'array
+ * @c : caractere dans l'array
+ * Return: NULL si fail, sinon l'array
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *array = malloc(size * sizeof(char));
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+	array[i] = c;
+	}
+	return (array);
+}
---
+++ malloc_free/README.md
@@ -0,0 +1,1 @@
+learning what malloc and free is
