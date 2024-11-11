d81aaa32837053f85f2ba8027ae3c119c25c37cc
Faf-dev <noaillesjulien@gmail.com>
2024-11-11 13:34:22 +0000

    fix task 0 without sizeof (don't know if we are allowed to do that)

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - copie la string donnee en parametre
+ * @str : string a copier
+ * Return: NULL si fail, str_cpy sinon
+ */
+char *_strdup(char *str)
+{
+	char *strcpy = malloc(
+}
