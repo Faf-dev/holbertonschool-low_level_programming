49d3a04afcc07da7fb140bfad5103e7efedfb61a
Faf-dev <noaillesjulien@gmail.com>
2024-11-13 09:03:28 +0000

    attempt task 0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,4 +1,13 @@
-struct dog my_dog
+#include "0-main.c"
+#include <stdio.h>
+/**
+ * dog - structure
+ * @name : nom du chien
+ * @age : age du chien
+ * @owner : maitre du chien
+ */
+
+struct dog
 {
 	char *name;
 	float age;
