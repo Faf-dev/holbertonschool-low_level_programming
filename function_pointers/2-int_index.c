7d222428468959baea7fdd3914ac61c727c094f0
root <root@DESKTOP-A00A4R8.>
2024-11-19 13:44:11 +0100

    push task 2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * int_index - cherche un entier
+ * @array: pointeur de tableau contenant des entiers
+ * @size: la taille du tableau
+ * @cmp: pointeur de fonction
+ * Return: i si trouvé, -1 si pas de match
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+
+int i;
+
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
+return (-1);
+}
