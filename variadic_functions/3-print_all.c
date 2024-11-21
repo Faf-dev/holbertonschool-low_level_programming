f469f7d093658f460e048abc5d5d5f27f4701f22
Faf-dev <noaillesjulien@gmail.com>
2024-11-21 15:50:43 +0100

    push task 3

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_all - print tout type d'arguments
+ * @format: arguments
+ */
+
+void print_all(const char * const format, ...)
+{
+	unsigned int i = 0;
+	char *str;
+	const char *separator = ", ";
+
+	va_list args;
+
+	va_start(args, format);
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+		case 'c':
+		printf("%c", va_arg(args, int));
+		break;
+		case 'i':
+		printf("%s%d", separator, va_arg(args, int));
+		break;
+		case 'f':
+		printf("%s%f", separator, va_arg(args, double));
+		break;
+		case 's':
+		str = va_arg(args, char *);
+		if (str == NULL)
+		printf("%s(nil)", separator);
+		printf("%s%s", separator, str);
+		break;
+		default:
+		i++;
+		continue;
+		}
+		i++;
+	}
+
+	va_end(args);
+	printf("\n");
+}
