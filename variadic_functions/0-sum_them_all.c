1dee9d0973e97fd06fad63b60b1de643be6ecd66
Faf-dev <noaillesjulien@gmail.com>
2024-11-21 13:25:05 +0100

    attempt to fix task 0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -12,10 +12,9 @@ int sum_them_all(const unsigned int n, ...)
 	int somme;
 	unsigned int i;
 	va_list add;
-
-	if (n == 0)
-	return (0);
-	va_start(add, n);
+		va_start(add, n);
+		if (n == 0)
+		return (0);
 		for (i = 0; i < n ; i++)
 		somme += va_arg(add, int);
 
