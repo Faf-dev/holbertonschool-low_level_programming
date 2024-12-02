6aca11432db5a54bd9d6050385457f4ebe3947c9
root <root@DESKTOP-A00A4R8.>
2024-12-02 15:18:30 +0100

    fix task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,7 +8,18 @@
 
 size_t print_list(const list_t *h)
 {
-	if (h == NULL)
-		printf("[0] (nil)");
-	return (h->len);
+	int count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+			else
+			{
+				printf("[%d] %s\n", h->len, h->str);
+			}
+			h = h->next;
+			count++;
+	}
+	return (count);
 }
