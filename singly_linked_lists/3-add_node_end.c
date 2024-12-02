0d4dcedefc7b03afc63bd46fde44b44b24002024
root <root@DESKTOP-A00A4R8.>
2024-12-02 16:19:52 +0100

    push task 3

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * add_node_end - add a node at the end of list_t
+ * @head: 
+ * @str: string (malloc'ed string)
+ * Return:  the address of the new element, or NULL if it failed
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newLastNode;
+	list_t *current;
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	newLastNode = malloc(sizeof(list_t));
+
+	if (newLastNode == NULL)
+	{
+		free (newLastNode);
+		return (NULL);
+	}
+	if (*head == NULL)
+	*head = newLastNode;
+	else
+	{
+	current = *head;
+	while (current->next != NULL)
+		current = current->next;
+	current->next = newLastNode;
+	}
+
+	newLastNode->str = strdup(str);
+	newLastNode->len = i;
+	
+	return (newLastNode);
+}
