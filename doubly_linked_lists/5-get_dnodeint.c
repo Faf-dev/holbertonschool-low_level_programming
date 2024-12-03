4355bb5a27393048c35b5b5f341de746c2688994
root <root@DESKTOP-A00A4R8.>
2024-12-03 13:40:56 +0100

    push task 5

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - create a note at the index
+ * @head: the first node
+ * @index: where the node should be
+ * Return: the nth node or NULL if it failed
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (i < index)
+	{
+		head = head->next;
+		i++;
+	}
+	return (head);
+}
