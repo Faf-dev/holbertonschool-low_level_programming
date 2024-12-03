ef0e8602c0daffa86a21f3c0aa8ac02e00140e85
root <root@DESKTOP-A00A4R8.>
2024-12-03 12:53:50 +0100

    push task 3

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - add a new node at the end of dlistint_t
+ * @head: the first node of dlistint_t
+ * @n: value of the node
+ * Return: adress of new node or NULL if failed
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *newNode = malloc(sizeof(dlistint_t));
+	dlistint_t *temp;
+
+
+	if (newNode == NULL)
+	{
+		free(newNode);
+		return (NULL);
+	}
+	if (*head == NULL)
+		*head = newNode;
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = newNode;
+		newNode->n = n;
+	}
+	return (newNode);
+}
