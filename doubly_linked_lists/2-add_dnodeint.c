a3fa193ee52a31367b20f3cbb4143c37b5e1b122
root <root@DESKTOP-A00A4R8.>
2024-12-03 13:08:27 +0100

    fix task 2, add ->prev for newnode and head

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -17,6 +17,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	newNode->n = n;
 	newNode->next = *head;
+	newNode->prev = NULL;
+	if (*head != NULL)
+		(*head)->prev = newNode;
 	*head = newNode;
 	return (newNode);
 }
