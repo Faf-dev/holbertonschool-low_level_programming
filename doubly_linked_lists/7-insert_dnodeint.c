aaeea7ff74250da19e20513dc3ae7f60096bc1dd
root <root@DESKTOP-A00A4R8.>
2024-12-03 14:43:24 +0100

    push task 7

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,57 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - create a node at a given position
+ * @h: the first node
+ * @idx: the given position where the new node should be
+ * @n: the value of the new node
+ * Return: the adress of the new node, NULL if it failed
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *newNode;
+	dlistint_t *temp;
+	unsigned int count = 0;
+	unsigned int i;
+
+	if (h == NULL)
+		return (NULL);
+	temp = *h;
+	while (temp->prev != NULL)
+		temp = temp->prev;
+	while (temp != NULL)
+	{
+		count++;
+		temp = temp->next;
+	}
+	if (idx > count)
+		return (NULL);
+
+	newNode = malloc(sizeof(dlistint_t));
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->n = n;
+
+	if (idx == 0)
+	{
+		newNode->next = *h;
+		newNode->prev = NULL;
+		if (*h != NULL)
+			(*h)->prev = newNode;
+		*h = newNode;
+		return (newNode);
+	}
+
+	temp = *h;
+
+	for (i = 0; i < idx - 1; i++)
+		temp = temp->next;
+	newNode->next = temp->next;
+	newNode->prev = temp;
+	if (temp->next != NULL)
+		temp->next->prev = newNode;
+	temp->next = newNode;
+	return (newNode);
+}
